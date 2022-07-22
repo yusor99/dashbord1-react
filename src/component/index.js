df6290e9d4cab6d61ada562fd46b46d284481f9a
yusor99 <44298686+yusor99@users.noreply.github.com>
2022-07-22 16:48:37 +0300

    Update index.js

--- src/component/index.js
+++ src/component/index.js
@@ -1,11 +1,7 @@
-/**import Categories from "./categories";
+import Categories from "./categories";
 import Navbar from "./navbar";
 import State from "./state";
-import NewProduct from "./product/newProduct";
-import ProductList from "./product/productList/productList.js";
-import CostumerList1 from "./costumers/costumersList/costumerList1";
-import CostumerList2 from "./costumers/costumersList/costumerList2";**/
-import NewCostumerForm from "./costumers/newCostumerForm";
+
 const Home = () => {
   return (
     <>
